c1b8ead8cfec16d787ef477cb61bafb1962569ba
Connor Duncan <connor@connorduncan.xyz>
2025-09-14 11:42:36 -0500

    Apply cargo fixes

--- src/user_config/mod.rs
+++ src/user_config/mod.rs
@@ -1,5 +1,5 @@
 use error::MordantConfigResult;
-use highlighter_options::{LanguageSrc, MordantHighlighterConfig};
+use highlighter_options::MordantHighlighterConfig;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::path::PathBuf;
